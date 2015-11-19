4cffbc1230e4c16b0a7648bb2898bef302fbbf09
Felix Rieseberg <felix@felixrieseberg.com>
2015-11-19 16:06:06 +0100

    Fix Broken Proxy Method

--- extension.ts
+++ extension.ts
@@ -1,7 +1,6 @@
 import {window, commands, workspace} from 'vscode';
 import TravisStatusIndicator from './travis-status';
 
-var globalTunnel = require('global-tunnel');
 var indicator, proxySetup;
 
 export function activate() { 
@@ -30,7 +29,7 @@ function updateStatus() {
 }
 
 function openInTravis() {
-	if (!proxySetup) setupProxy();5
+	if (!proxySetup) setupProxy();
 	indicator = indicator || new TravisStatusIndicator();
 	indicator.openInTravis();
 }
@@ -40,17 +39,23 @@ function setupProxy() {
 		// Seems like we have a proxy
 		let match = process.env.http_proxy.match(/^(http:\/\/)?([^:\/]+)(:([0-9]+))?/i);
 		
-		if (match) {
+		if (match && match.length >= 3) {
 			this._proxyData.host = match[2];
-			this._proxyData.port = (match[4] != null ? match[4] : 80);
+			this._proxyData.port = match[4] != null ? match[4] : 80;
+		}
+		
+		if (this._proxyData.host && this._proxyData.port) {
+			var globalTunnel = require('global-tunnel');
 
 			globalTunnel.initialize({
 				host: this._proxyData.host,
 				port: this._proxyData.port
-			});
+			});	
 		} else {
 			// We have trouble getting ifnormation form the global http_proxy env variable
 			window.showErrorMessage('Travis CI: HTTP Proxy settings detected, but we have trouble parsing the setting. The extension may not work properly.');
 		}
 	}
+	
+	proxySetup = true;
 }
